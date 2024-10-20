8e28bd9fd36b69ed72aa5aa1b9669fba51d0dfd3
Subhaharini733 <subhaharini733@gmail.com>
2024-10-21 01:43:41 +0530

    Create cal.js

---
+++ Value Added Course/React/Calculator/cal.js
@@ -0,0 +1,12 @@
+function add(){
+    let a = parseInt(document.getElementById('n1').value);
+    let b = parseInt(document.getElementById('n2').value);
+    let c = a+b;
+    document.getElementById("b1").innerHTML="The addition of two numbers:"+c;
+}
+function sub(){
+    let a = parseInt(document.getElementById('n1').value);
+    let b = parseInt(document.getElementById('n2').value);
+    let c = a+b;
+    document.getElementById("b2").innerHTML="The subtraction of two numbers:"+c;
+}
